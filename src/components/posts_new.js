fe3b6ce5ed75461ff0bafe4e28746577601055e3
sjxu2 <36207519+sjxu2@users.noreply.github.com>
2018-08-27 16:53:49 -0400

    Add files via upload

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,15 +1,95 @@
 import React, {Component} from 'react';
+import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createPost } from "../actions/index";
 
 class PostsNew extends Component
 {
+    renderField(field)
+    {
+        const {meta:{error,touched}} = field;
+        const className =`form-group ${touched&&error?'has-danger':''}`;
+
+        return(
+            <div className={className}>
+                <label>{field.label}</label>
+                <input className='form-control' 
+                type='text' 
+                {...field.input} >
+                </input>
+                <div className='text-help'>
+                    {touched?error:''}
+                </div>
+            </div>
+        )
+    }
+
+    onSubmit(values)
+    {
+        this.props.createPost(values);
+    }
+
     render()
     {
+
+        const {handleSubmit}= this.props;
+
         return(
-            <div>
-                hola!
-            </div>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field 
+                name='title' 
+                label='TITLE'
+                component={this.renderField}>
+                </Field>
+
+                <Field 
+                name='categories'
+                label='CATEGORIES'
+                component={this.renderField}>
+                </Field>
+
+                <Field 
+                name='content'
+                label='POST CONTENT'
+                component={this.renderField}>
+                </Field>
+
+                <button type='submit' className='btn btn-primary'>
+                    SUBMIT
+                </button>
+                <Link to='/' className='btn btn-danger'>CANCEL
+                </Link>
+            </form>
         );
     }
 }
 
-export default PostsNew;
+function validate(values)
+{
+    const errors={};
+
+    if(!values.title)
+    {
+        errors.title='Please give a title';
+    }
+
+    if(!values.categories)
+    {
+        errors.categories='Please give a category';
+    }
+
+    if(!values.content)
+    {
+        errors.content='Please give some content';
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form:'PostsNewForm'
+})(
+   connect(null,{createPost}) (PostsNew)
+);
